430d6c947ad485fe926176a9edef47c1af98d966
Léo Germond <leo.germond@gmail.com>
2017-05-02 19:08:57 +0200

    add loading function (wip)

--- coiot_device.py
+++ coiot_device.py
@@ -1,5 +1,24 @@
 import threading
 
+def load_coiot_devices(db, drivers):
+    """
+    Initialize devices
+
+    First look into database to get all saved devices, then connect each one to the right
+    driver; finally return them for the caller to create the DBus interfaces
+    """
+    db_devices = db.devices
+    devices = []
+    for drivername in db_devices:
+        assert(drivername in drivers)
+
+        for db_device in db_devices[drivername]:
+            device = CoiotDevice(db_device)
+            drivers[drivername].connect(device)
+            devices.append(device)
+
+    return devices
+
 class CoiotDevice:
     """
     This object acts in the intersection between the driver object (that actually
