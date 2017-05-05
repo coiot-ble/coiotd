2c833ba5f908eb97860cfdbb031116e8e1c0c70c
Léo Germond <leo.germond@gmail.com>
2017-05-05 18:36:32 +0200

    test, fix and start integrating dbus

--- coiot_dbus.py
+++ coiot_dbus.py
@@ -18,63 +18,21 @@ INTROSPECTABLE_DBUS = """
 
 
 class DBusDevice:
-    """
-    Generic class to use in order to declare a device on DBus.
-    Instances of that class act as a dictionnary to give access to
-    COIoT specific attributes. This allows for properties to be
-    discovered at runtime through a generic mechanism.
-    """
-    def __init__(self, *interfaces):
+    def __init__(self, proxy, *interfaces):
+        self.proxy = proxy
         self.interfaces = interfaces
+        CoiotDBus.instance.register_device(self)
 
-    def keys(self):
-        return []
+    def __setattr__(self, key, value):
+        if key[0].islower():
+            object.__setattr__(self, key, value)
+            return
+        setattr(self.proxy, key, value)
 
-    def items(self):
-        return {n: getattr(self, n) for n in self.keys()}.items()
-
-    def __setitem__(self, key, value):
-        if key not in self.keys():
-            raise KeyError()
-        setattr(self, key, value)
-
-    def __getitem__(self, key):
-        if key not in self.keys():
-            raise KeyError()
-        getattr(self, key)
-
-    def __iter__(self):
-        return iter(self.keys())
-
-
-class DBusDisplayable(DBusDevice):
-    def __init__(self, *interfaces):
-        super().__init__(COIOT_DISPLAYABLE1_DBUS, *interfaces)
-
-    def keys(self):
-        return ["Name", "Type"].extend(super().keys())
-
-    @property
-    def Name(self):
-        return type(self).__name__
-
-
-class DBusLamp(DBusDisplayable):
-    def __init__(self, device):
-        super().__init__(COIOT_SWITCHABLE1_DBUS)
-        self.Type = "Lamp"
-        self.device = device
-
-    def keys(self):
-        return ["On"].extend(super().keys())
-
-    @property
-    def On(self):
-        return self.device.on
-
-    @On.setter
-    def On(self, value):
-        self.device.on = value
+    def __getattr__(self, key):
+        if key[0].islower():
+            return object.__getattr__(self, key)
+        return getattr(self.proxy, key)
 
 
 class CoiotDBusDeviceInterface:
@@ -83,16 +41,20 @@ class CoiotDBusDeviceInterface:
         self.path = "/org/coiot/" + path
         self.dbus = '<node>{}</node>'.format("".join(device.interfaces))
 
+    def register_on(self, bus):
+        return bus.register_object(self.device, self.path, self.dbus)
+
 
 class CoiotDBus:
-    def __init__(self, bus, client):
-        self.client = client
+    instance = None
+
+    def __init__(self, bus):
+        self.bus = bus
         root_interface = ('/org/coiot', self,
                        "<node>{}</node>".format(INTROSPECTABLE_DBUS))
         self.publication = self.bus.publish('org.coiot', root_interface)
-        self.register_devices()
+        CoiotDBus.instance = self
 
-    def register_devices(self):
-        for i, d in enumerate(self.client.devices.values()):
-            d = self.bus.register_object(CoiotDBusDeviceInterface(d, str(i)))
-            self.publication._at_exit(d.__exit__)
+    def register_device(self, device):
+        d = CoiotDBusDeviceInterface(device, device.ID).register_on(self.bus)
+        self.publication._at_exit(d.__exit__)
--- test/test_dbus.py
+++ test/test_dbus.py
@@ -1,6 +1,36 @@
 #! /usr/bin/env python
-import pydbus
+from coiot_dbus import DBusDevice, CoiotDBus, COIOT_DISPLAYABLE1_DBUS
+import unittest
+from unittest.mock import Mock
 
-if __name__ == "__main__":
-    bus = pydbus.SystemBus()
-    help(bus.get("org.coiot", "/org/coiot/0"))
+
+class CoiotDBusBasicTest(unittest.TestCase):
+    """
+    Very basic DBus tests, the goal is not to test that the DBus interface
+    is correct, but rather that it correctly interfaces with the underlying
+    CoiotDevice (straightforward set or get, displays correct devices).
+    Testing the DBus interface [will be] done through a fully-featured
+    COIoT DBus client.
+    """
+    def setUp(self):
+        self.bus = Mock()
+        self.bus.register_object.return_value = Mock()
+        self.bus.register_object.return_value.__enter__ = Mock(return_value=(Mock(), None))
+        self.bus.register_object.return_value.__exit__ = None
+
+        self.device = Mock()
+        self.device.ID = "1"
+        self.device.Name = "foo"
+        self.device.Type = "Bar"
+
+        self.coiot_bus = CoiotDBus(self.bus)
+        self.dbus_device = DBusDevice(self.device, COIOT_DISPLAYABLE1_DBUS)
+
+    def test_setup(self):
+        self.bus.publish.assert_called_once()
+        self.assertEqual('org.coiot', self.bus.publish.call_args[0][0])
+
+        self.bus.register_object.assert_called_once()
+        register_args = self.bus.register_object.call_args[0]
+        self.assertEqual(self.dbus_device, register_args[0])
+        self.assertEqual('/org/coiot/1', register_args[1])
