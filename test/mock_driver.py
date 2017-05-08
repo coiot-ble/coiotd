8931ce91cf07e9a538e2f763ccaba54959aa8fe1
Léo Germond <leo.germond@gmail.com>
2017-05-08 11:53:28 +0200

    mock driver must be explicitely registered

--- test/mock_driver.py
+++ test/mock_driver.py
@@ -5,14 +5,16 @@ import unittest.mock
 log = logging.getLogger('Test')
 
 
-def register():
-    @coiot.db.CoiotDBInterface.declare
-    class MockDriverDevice:
+class MockDriverDevice:
 
-        @classmethod
-        def load(Cls, self):
-            self.driver = unittest.mock.Mock()
-            self.list = {}
-            self.driver.connect = lambda d: d.add_action_list(self.list)
-            log.info('Mock driver loaded for {}'.format(self))
-            return True
+    @classmethod
+    def load(Cls, self):
+        self.driver = unittest.mock.Mock()
+        self.list = {}
+        self.driver.connect = lambda d: d.add_action_list(self.list)
+        log.info('Mock driver loaded for {}'.format(self))
+        return True
+
+
+def register():
+    coiot.db.CoiotDBInterface.declare(MockDriverDevice)
